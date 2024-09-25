40121c82046332c70eca4613a207da73d58c4884
S1ro1 <matej.sirovatka@gmail.com>
2024-09-26 01:13:55 +0200

    Wip: add docker compose creation

--- app/main.py
+++ app/main.py
@@ -1,8 +1,11 @@
+import os
+import yaml
 from fastapi import FastAPI, UploadFile, File
 from fastapi.responses import JSONResponse
-from .utils import process_zip_file
+from .utils import docker_compose_up, git_pull, process_zip_file, read_bitswan_yaml
 
 app = FastAPI()
+
 
 @app.post("/create/{deployment_id}")
 async def upload_zip(deployment_id: str, file: UploadFile = File(...)):
@@ -13,3 +16,78 @@ async def upload_zip(deployment_id: str, file: UploadFile = File(...)):
         return JSONResponse(
             content={"error": "File must be a ZIP archive"}, status_code=400
         )
+
+
+@app.get("/deploy")
+async def deploy():
+    bitswan_dir = os.environ.get("BS_BITSWAN_DIR", "/mnt/repo/pipeline")
+    bitswan_yaml_path = os.path.join(bitswan_dir, "bitswan.yaml")
+
+    if not await git_pull(bitswan_dir):
+        return JSONResponse(
+            content={"error": "Error pulling from git"}, status_code=500
+        )
+
+    try:
+        bs_yaml = read_bitswan_yaml(bitswan_yaml_path)
+    except Exception as e:
+        return JSONResponse(content={"error": e}, status_code=500)
+
+    dc = {
+        "version": "3",
+        "services": {},
+        "networks": {
+            network: {"external": True}
+            for network in bs_yaml.get("default-networks", {})
+        },
+    }
+    deployments = bs_yaml.get("deployments", {})
+    for deployment_id, conf in deployments.items():
+        conf = conf or {}
+        entry = {}
+
+        entry["environment"] = {"DEPLOYMENT_ID": deployment_id}
+        entry["container_name"] = deployment_id
+        entry["restart"] = "always"
+        entry["labels"] = {
+            "gitops.deployment_id": deployment_id,
+        }
+
+        if ("env_dir" in bs_yaml) and os.path.exists(
+            env_file := os.path.join(bs_yaml["env_dir"], deployment_id)
+        ):
+            entry["env_file"] = env_file
+        if os.path.exists(env_file := os.path.join(bs_yaml["env_dir"], "default")):
+            entry["env_file"] = env_file
+
+        if "network_mode" in conf:
+            entry["network_mode"] = conf["network_mode"]
+        elif "networks" in conf:
+            entry["networks"] = conf["networks"].copy()
+        elif "default-networks" in bs_yaml:
+            entry["networks"] = bs_yaml["default-networks"].copy()
+
+        passthroughs = ["volumes", "ports", "devices", "container_name"]
+        entry.update({p: conf[p] for p in passthroughs if p in conf})
+
+        source = conf.get("source", deployment_id)
+        deployment_dir = os.path.join(bitswan_dir, source)
+
+        entry["image"] = "bitswan/pipeline-runtime-environment:latest"
+        if "volumes" not in entry:
+            entry["volumes"] = []
+        entry["volumes"].append(f"{deployment_dir}:/conf")
+
+        if conf.get("enabled", True):
+            dc["services"][deployment_id] = entry
+
+    dc_yaml = yaml.dump(dc)
+
+    deployment_result = await docker_compose_up(bitswan_dir, dc_yaml, deployments)
+
+    if any([result["return_code"]] for result in deployment_result.values()):
+        return JSONResponse(
+            content={"error": "Error deploying services"}, status_code=500
+        )
+
+    return JSONResponse(content={"message": "Services deployed successfully"})
--- app/utils.py
+++ app/utils.py
@@ -1,8 +1,13 @@
+import asyncio
 import hashlib
 import os
+from typing import Any
 import zipfile
 from tempfile import NamedTemporaryFile
+import docker
+from dockerfile_parse import DockerfileParser
 import yaml
+
 
 async def process_zip_file(file, deployment_id):
     with NamedTemporaryFile(delete=False) as temp_file:
@@ -14,29 +19,136 @@ async def process_zip_file(file, deployment_id):
 
     try:
         os.makedirs(output_dir, exist_ok=True)
-        with zipfile.ZipFile(temp_file.name, 'r') as zip_ref:
+        with zipfile.ZipFile(temp_file.name, "r") as zip_ref:
             zip_ref.extractall(output_dir)
-        
-        bitswan_home = os.environ.get('BITSWAN_HOME', '')
+
+        bitswan_home = os.environ.get("BS_BITSWAN_DIR", "/mnt/repo/bitswan")
         bitswan_yaml_path = os.path.join(bitswan_home, "bitswan.yaml")
-        
+
         # Update or create bitswan.yaml
         if os.path.exists(bitswan_yaml_path):
-            with open(bitswan_yaml_path, 'r') as f:
+            with open(bitswan_yaml_path, "r") as f:
                 data = yaml.safe_load(f) or {}
         else:
             data = {}
-        
+
         data[checksum] = deployment_id
-        
-        with open(bitswan_yaml_path, 'w') as f:
+
+        with open(bitswan_yaml_path, "w") as f:
             yaml.dump(data, f)
-        
-        return {"message": "File processed successfully", "output_directory": output_dir, "checksum": checksum}
+
+        return {
+            "message": "File processed successfully",
+            "output_directory": output_dir,
+            "checksum": checksum,
+        }
     except Exception as e:
         return {"error": f"Error processing file: {str(e)}"}
     finally:
         os.unlink(temp_file.name)
+
+
+def read_bitswan_yaml(bitswan_yaml_path: str) -> dict[str, Any]:
+    try:
+        if os.path.exists(bitswan_yaml_path):
+            with open(bitswan_yaml_path, "r") as f:
+                bs_yaml: dict = yaml.safe_load(f)
+                return bs_yaml
+        else:
+            raise FileNotFoundError("bitswan.yaml not found")
+    except Exception as e:
+        raise Exception(f"Error reading bitswan.yaml: {str(e)}")
+
+
+async def docker_compose_up(
+    bitswan_dir: str, docker_compose: str, deployment_info: dict[str, Any]
+) -> None:
+    services_to_recreate = [
+        svc
+        for svc, info in deployment_info.items()
+        if info[
+            "recreate"
+        ]  # FIXME: should be always True and not specified in the info
+    ]
+
+    ide_services = [svc + "__ide__" for svc in deployment_info.keys()]
+
+    async def setup_asyncio_process(cmd: list[Any]) -> dict[str, Any]:
+        proc = await asyncio.create_subprocess_exec(
+            *cmd,
+            stdin=asyncio.subprocess.PIPE,
+            stdout=asyncio.subprocess.PIPE,
+            stderr=asyncio.subprocess.PIPE,
+            cwd=bitswan_dir,
+        )
+
+        stdout, stderr = await proc.communicate(input=docker_compose.encode())
+
+        return {
+            "cmd": cmd,
+            "stdout": stdout.decode("utf-8"),
+            "stderr": stderr.decode("utf-8"),
+            "returncode": proc.returncode,
+        }
+
+    build_result = None
+    if services_to_recreate:
+        build_result = await setup_asyncio_process(
+            ["docker-compose", "-f", "/dev/stdin", "--pull"].extend(
+                services_to_recreate
+            )
+        )
+
+    up_result = await setup_asyncio_process(
+        ["docker-compose", "-f", "/dev/stdin", "up", "-d", "--remove-orphans"]
+    )
+    ide_result = await setup_asyncio_process(
+        ["docker-compose", "-f", "/dev/stdin", "up", "--no-start"].extend(ide_services)
+    )
+
+    return {
+        "build_result": build_result,
+        "up_result": up_result,
+        "ide_result": ide_result,
+    }
+
+
+async def git_pull(bitswan_dir: str) -> bool:
+    abspath = os.path.abspath(bitswan_dir)
+
+    await asyncio.create_subprocess_exec(
+        "git", "config", "--global", "--add", "safe.directory", abspath
+    )
+
+    await asyncio.create_subprocess_exec(
+        "git", "config", "pull.rebase", "false", cwd=bitswan_dir
+    )
+
+    pull_proc = await asyncio.create_subprocess_exec("git", "pull", cwd=bitswan_dir)
+    await pull_proc.wait()
+
+    if pull_proc.returncode:
+        return False
+
+    return True
+
+
+def manage_containers(
+    bitswan_dir: str, git_root: str, bs_yaml: dict[str, Any]
+) -> dict[str, Any]:
+    parser = DockerfileParser()
+    client = docker.from_env()
+
+    deployment_info = (
+        get_deployment_info(client, deployments)
+        if (deployments := bs_yaml.get("deployments"))
+        else {}
+    )
+
+    for deployment_id, info in deployment_info.items():
+        if info["running"]:
+            conf = deployments[deployment_id]
+
 
 def calculate_checksum(file_path):
     sha256_hash = hashlib.sha256()
