a6e9c4709a9d473151fc157b23633cdcdd3b51ee
Lukas Vecerka <vecerkalukas18@gmail.com>
2024-12-06 10:29:45 +0100

    secured all gitops endpoint with Bearer token

---
+++ app/dependencies.py
@@ -0,0 +1,13 @@
+import os
+from fastapi import HTTPException, Security
+from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
+
+
+def verify_token(credentials: HTTPAuthorizationCredentials = Security(HTTPBearer())):
+    secret_token = os.environ.get("BS_GITOPS_SECRET")
+    if credentials.scheme != "Bearer" or credentials.credentials != secret_token:
+        raise HTTPException(
+            status_code=401,
+            detail="Unauthorized: Invalid or missing token",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
--- app/main.py
+++ app/main.py
@@ -1,10 +1,11 @@
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI
 
 from .routes.create_deployment import router as create_deployment_router
 from .routes.deploy import router as deploy_router
 from .routes.list_pres import lifespan
+from .dependencies import verify_token
 
-app = FastAPI(lifespan=lifespan)
+app = FastAPI(lifespan=lifespan, dependencies=[Depends(verify_token)])
 
 app.include_router(create_deployment_router)
 app.include_router(deploy_router)
