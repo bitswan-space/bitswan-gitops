52bd606b2f7606ba30e7d68dd30c6f428204ce3c
Lukas Vecerka <vecerkalukas18@gmail.com>
2025-04-29 10:19:01 +0200

    fix wrong property name

--- app/services/automation_service.py
+++ app/services/automation_service.py
@@ -192,7 +192,7 @@ class AutomationService:
         return {"status": "success", "message": message}
 
     async def deploy_automation(self, deployment_id: str):
-        os.environ["COMPOSE_PROJECT_NAME"] = self.gitops_id
+        os.environ["COMPOSE_PROJECT_NAME"] = self.get_workspace_name()
 
         bs_yaml = read_bitswan_yaml(self.gitops_dir)
 
@@ -216,7 +216,7 @@ class AutomationService:
         }
 
     async def deploy_automations(self):
-        os.environ["COMPOSE_PROJECT_NAME"] = self.gitops_id
+        os.environ["COMPOSE_PROJECT_NAME"] = self.get_workspace_name()
 
         bs_yaml = read_bitswan_yaml(self.gitops_dir)
 
