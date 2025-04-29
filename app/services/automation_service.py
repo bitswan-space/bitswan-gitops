dffbc613e0bf330573232d4548ddbd262bb6217b
Lukas Vecerka <vecerkalukas18@gmail.com>
2025-04-29 12:00:53 +0200

    blacken

--- app/services/automation_service.py
+++ app/services/automation_service.py
@@ -99,7 +99,7 @@ class AutomationService:
                     "gitops.intended_exposed", "false"
                 )
 
-                url = generate_url(deployment_id,gitops_domain,True)
+                url = generate_url(deployment_id, gitops_domain, True)
 
                 if label != "true":
                     url = None
